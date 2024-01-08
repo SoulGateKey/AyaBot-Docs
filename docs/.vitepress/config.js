fe66c7f3fccbb34b9c7452b03bcbca06e819a611
SoulGateKey <soulgatekey@gmail.com>
2024-01-08 21:38:56 +0800

    Update Vitepress Config

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,10 +1,12 @@
 // .vitepress/config.js
 export default {
     // 站点级选项
+    lang: 'zh-CN',
     title: 'AyaBot Documentation',
     description: '绫Bot 使用文档',
-  
+    head: [['link', { rel: 'icon', href: '/favicon.ico'}]],
     themeConfig: {
       // 主题级选项
+      lastUpdated: true
     }
   }
